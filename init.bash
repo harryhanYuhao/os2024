2f458a081209b607add6ab08acc30f469304a051
Harry Han <s2162783@ed.ac.uk>
2024-01-30 22:59:58 +0000

    runqemu

--- init.bash
+++ init.bash
@@ -3,7 +3,11 @@
 cp brc ~/.brc 
 
 mkdir -p ~/.local/bin/
+
 cp killQemu.bash ~/.local/bin/killqemu
-chmod +X ~/.local/bin/killqemu
+chmod +x ~/.local/bin/killqemu
+
+cp runqemu ~/.local/bin/runqemu
+chmod +x ~/.local/bin/runqemu
 
 source ~/.brc
